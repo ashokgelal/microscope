6a02d1ed0c38d28d59d186dd5cd5d913ec3262c8
Ashok Gelal <ashokgelal@gmail.com>
2015-03-01 18:18:18 -0700

    Allow to add comments

--- microscope/server/fixtures.js
+++ microscope/server/fixtures.js
@@ -1,16 +1,56 @@
 if (Posts.find().count() === 0) {
-    Posts.insert({
+    var now = new Date().getTime();
+
+    var meId = Meteor.users.insert({
+        profile: {name: 'Ashok Gelal'}
+    });
+
+    var me = Meteor.users.findOne(meId);
+    var youId = Meteor.users.insert({
+        profile: {name: 'Mr You'}
+    });
+    var you = Meteor.users.findOne(youId);
+
+    var telescopeId = Posts.insert({
         title: 'Introducing Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
+        userId: you._id,
+        author: you.profile.name,
+        url: 'http://telescope.com',
+        submitted: new Date(now - 7 * 3600 * 1000),
+        commentsCount: 2
+    });
+
+    Comments.insert({
+        postId: telescopeId,
+        userId: me._id,
+        author: me.profile.name,
+        submitted: new Date(now - 5 * 3600 * 1000),
+        body: 'Interesting project, can I get involved?'
+    });
+
+    Comments.insert({
+        postId: telescopeId,
+        userId: you._id,
+        author: you.profile.name,
+        submitted: new Date(now - 3 * 3600 * 1000),
+        body: 'You sure can!'
     });
 
     Posts.insert({
         title: 'Meteor',
-        url: 'http://meteor.com'
+        userId: me._id,
+        author: me.profile.name,
+        url: 'http://meteor.com',
+        submitted: new Date(now - 10 * 3600 * 1000),
+        commentsCount: 0
     });
 
     Posts.insert({
         title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
+        userId: me._id,
+        author: me.profile.name,
+        url: 'http://themeteorbook.com',
+        submitted: new Date(now - 12 * 3600 * 1000),
+        commentsCount: 0
     });
 }
